72144300b389cfa508514f67a7ad6704e0aa6732
benjamin dange <benjamin.dange@gmail.com>
2020-02-04 13:39:52 -0500

    Refactored header with logo only

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,9 @@
 import React from "react";
 import Logo from "../logo.png";
-import { SocialIcon } from "react-social-icons";
 
 function Header() {
   return (
-    <ul className="header">
-      <li className="flex-item container">
-        {/* <div className="social-left">
-          <SocialIcon
-            url="https://www.instagram.com/cestafric/"
-            target="_blank"
-          />
-        </div>
-        <div className="social">
-          <SocialIcon
-            url="https://www.facebook.com/cestafric/"
-            target="_blank"
-          />
-        </div> */}
-      </li>
-      <li className="flex-item">
-        <img
-          id="logo"
-          style={{ height: 150 }}
-          src={Logo}
-          alt="Website Logo"
-        ></img>
-      </li>
-      <li className="flex-item right">{/* <h1>Links</h1> */}</li>
-    </ul>
+    <img id="logo" style={{ height: 150 }} src={Logo} alt="Website Logo"></img>
   );
 }
 
